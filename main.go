cc97face3369ca54d9e6cce6b9e34e91582faeb2
yusuf <yusufsyah01@gmail.com>
2023-03-16 21:47:07 +0700

    challange 4

--- main.go
+++ main.go
@@ -2,34 +2,50 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"os"
+	"strconv"
 )
 
+// struct untuk menyimpan data teman
+type Teman struct {
+    Absen   int
+    Nama    string
+    Alamat  string
+    Pekerjaan string
+    Alasan  string
+}
+
+// function untuk menampilkan data teman berdasarkan nomor absen
+func tampilkanDataTeman(absen int, daftarTeman []Teman) {
+    for _, teman := range daftarTeman {
+        if teman.Absen == absen {
+            fmt.Printf("Nama: %s\nAlamat: %s\nPekerjaan: %s\nAlasan memilih kelas Golang: %s\n", teman.Nama, teman.Alamat, teman.Pekerjaan, teman.Alasan)
+            return
+        }
+    }
+    fmt.Println("Data teman dengan nomor absen tersebut tidak ditemukan.")
+}
+
 func main() {
+    // data teman
+    daftarTeman := []Teman{
+        {1, "John", "Jakarta", "Software Engineer", "Ingin mempelajari bahasa pemrograman Golang"},
+        {2, "Jane", "Bogor", "Data Analyst", "Ditugaskan untuk mempelajari Golang untuk proyek perusahaan"},
+        {3, "Bob", "Tangerang", "IT Support", "Ingin meningkatkan skill programming dan memilih Golang sebagai bahasa baru yang dipelajari"},
+    }
 
-   
-    fmt.Println("Challange 3|| Yusuf Syahputra || 1955617840-478")
-   // Input kalimat
-	kalimat := "selamat malam"
+    // memeriksa argumen yang diberikan pada command line
+    if len(os.Args) < 2 {
+        fmt.Println("Penggunaan: go run biodata.go <nomor_absen>")
+        return
+    }
+    absenStr := os.Args[1]
+    absen, err := strconv.Atoi(absenStr)
+    if err != nil {
+        fmt.Println("Nomor absen harus berupa bilangan bulat.")
+        return
+    }
 
-	// Konversi kalimat menjadi lowercase agar tidak terpengaruh huruf besar atau kecil
-	kalimat = strings.ToLower(kalimat)
-
-
-	// Deklarasi map untuk menyimpan jumlah kemunculan huruf
-	hurufCount := make(map[rune]int)
-
-	// Looping untuk setiap karakter dalam kalimat
-	for _, karakter := range kalimat {
-      // mencetak karakter 
-      fmt.Printf("%c \n", karakter)
-		// Tambahkan jumlah kemunculan huruf dalam map
-		hurufCount[karakter]++
-	}
-
-	// Tampilkan hasil perhitungan kemunculan huruf
-	fmt.Println("Hasil perhitungan kemunculan huruf:")
-	for huruf, count := range hurufCount {
-		fmt.Printf("%c: %d ,", huruf, count)
-	}
+    // memanggil function untuk menampilkan data teman berdasarkan nomor absen yang diberikan
+    tampilkanDataTeman(absen, daftarTeman)
 }
